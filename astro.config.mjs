98808ae4707902c4d219b2eb817ef2926ab97a27
brunomont <brunoadrian.mont@gmail.com>
2023-02-06 15:01:27 -0500

    chore: modify astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://Fabz2002.github.io',
-  base: './',
   output: "server",
   adapter: vercel()
 });
