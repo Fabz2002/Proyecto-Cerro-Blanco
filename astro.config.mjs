5f9990cfa3981638f83b00e7e08ad9120e353f0a
Fabrizio Diaz <fabriziojdn02@gmail.com>
2023-02-06 14:31:09 -0500

    darktheme off for now , before deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://Fabz2002.github.io',
-	base: './Proyecto-Cerro-Blanco-Astro'
+	site: 'https://Fabz2002.github.io'
 })
