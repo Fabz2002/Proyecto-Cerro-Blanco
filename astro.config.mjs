55c30d491d2a069403a77cce13cc6bb92b76b099
brunomont <brunoadrian.mont@gmail.com>
2023-02-06 13:54:23 -0500

    feat: add vercel plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://Fabz2002.github.io',
-	base: './Proyecto-Cerro-Blanco-Astro'
-})
+  // site: 'https://Fabz2002.github.io',
+  base: './',
+  output: "server",
+  adapter: vercel()
+});
